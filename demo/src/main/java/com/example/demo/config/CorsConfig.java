3a10346294f2d23d6aaa331254200f1dace5af5c
Johann1254 <167133455+Johann1254@users.noreply.github.com>
2025-11-27 18:27:05 -0500

    cambios (agregado el cors)

---
+++ demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://costusoftcontrol-bue9amgehna0bwgb.brazilsouth-01.azurewebsites.net", 
+                                "http://localhost:4200",
+                                "http://localhost:3000",
+                                "http://localhost:8080",
+                                "http://localhost:59722",
+                                "http://localhost:52765"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
